b08bc1c6f123b4b3233cd06dfadd9990d976d34b
Simon Chervenak <sarras305@gmail.com>
2022-06-21 09:20:13 -0400

    make square faces

--- mirrormaker.py
+++ mirrormaker.py
@@ -9,13 +9,14 @@ with open("test_sword.txt") as f:
 real_vertices = []
 faces = []
 for v in vertices:
-    # each vertex gets two triangles
+    # each vertex gets a square around it
     x, y = v
     sq = [(x, y, 0), (x + 1, y, 0), (x, y + 1, 0), (x + 1, y + 1, 0)]  # todo z
 
     l = len(real_vertices)
-    faces.append((l + 1, l + 2, l + 3))
-    faces.append((l + 2, l + 3, l + 4))
+    faces.append((l + 1, l + 2, l + 4, l + 3))
+    # faces.append((l + 1, l + 2, l + 3))
+    # faces.append((l + 2, l + 3, l + 4))
 
     real_vertices.extend(sq)
 
@@ -24,4 +25,4 @@ with open(file, "w") as file:
     for v in real_vertices:
         file.write("v {} {} {}\n".format(*v))
     for f in faces:
-        file.write("f {} {} {}\n".format(*f))
+        file.write("f {} {} {} {}\n".format(*f))
