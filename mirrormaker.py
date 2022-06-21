251f28779ffc875fd3f8d8b51e9ed3a66597de0f
Simon Chervenak <sarras305@gmail.com>
2022-06-21 09:44:58 -0400

    interview question complete

--- mirrormaker.py
+++ mirrormaker.py
@@ -1,10 +1,39 @@
+from collections import defaultdict
+
+
+def find_regions(s):
+    regions = defaultdict(list)
+    s = s.split("\n")
+    for x, line in enumerate(s):
+        for y, char in enumerate(line):
+            if char != '.':
+                start = (x, y)
+                # assume the region is right-rectangular (todo: unassume this)
+                # also assume the region never hits hte edge
+                a, b = x, y
+                while s[a][b] == char:
+                    a += 1
+                a -= 1
+                while s[a][b] == char:
+                    b += 1
+                b -= 1
+
+                if not any(r[1] == (a, b) for r in regions[char]):
+                    regions[char].append((start, (a, b)))
+    return regions
+
+
 vertices = []
 
 with open("test_sword.txt") as f:
-    for x, line in enumerate(f):
+    s = f.read()
+    for x, line in enumerate(s.split("\n")):
         for y, ch in enumerate(line):
             if ch in '123456789':
                 vertices.append((x, y, int(ch)))
+
+    regions = find_regions(s)
+    print(regions)
 
 real_vertices = []
 faces = []
@@ -20,6 +49,9 @@ for v in vertices:
         faces.append((l + 1, l + 2, l + 4, l + 3))
 
         real_vertices.extend(sq)
+
+for depth, region in regions.items():
+    ...
 
 file = "test_sword.obj"
 with open(file, "w") as file:
